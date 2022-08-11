2fc2af6c06842b8395f9f0d06e9a05cdf1a24b6b
Duck <61479436+duck-dev@users.noreply.github.com>
2022-08-11 18:40:56 +0200

    Add serialization of data to files before benchmark
    - Before the deserialization can be benchmarked, there has to be something that can be deserialized. Overall 8 files are created with different content (one file for each entity in each method of testing (Integers, Objects)). This content will then be deserialized during the benchmarking process. This has already been done before, but inside one file for the "extra large" entity only.

--- src/Utilities.cs
+++ src/Utilities.cs
@@ -1,13 +1,30 @@
+using System.Text.Json;
+
 namespace JsonBenchmark;
 
 public static class Utilities
 {
     public const string FilePathIntegers = "Integers.json";
     public const string FilePathObjects = "Objects.json";
-    
+
     public static void FillJson(NewtonsoftVsSystem instance)
     {
-        instance.SaveIntegersSystem(Entities.ExtraLargePrimitives);
-        instance.SaveObjectsSystem(Entities.ExtraLargeObjects);
+        IReadOnlyList<List<int>> integers = instance.Integers().ToList();
+        IReadOnlyList<List<TestObject>> objects = instance.Objects().ToList();
+        
+        IReadOnlyList<string> integerPaths = instance.IntegerPaths().ToList();
+        IReadOnlyList<string> objectPaths = instance.ObjectPaths().ToList();
+
+        for(int i = 0; i < integers.Count; i++)
+            SaveData(integerPaths[i], integers[i]);
+        for(int i = 0; i < objects.Count; i++)
+            SaveData(objectPaths[i], objects[i]);
+    }
+
+    private static void SaveData<T>(string path, List<T> list)
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        string jsonString = JsonSerializer.Serialize(list, options);
+        File.WriteAllText(path, jsonString);
     }
 }
